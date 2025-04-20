a894a4f48618f048af3c413bc0432929e18fec10
megpeer <148973963+megpeer@users.noreply.github.com>
2025-04-20 18:10:57 -0400

    added event model for posting to index.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,26 @@
 class EventsController < ApplicationController
   def index
-    @event = event.new
-    @events = event.all.order(created_at: :asc)
+    @event = Event.new
+    @events = Event.all.order(created_at: :desc)
+  end
+
+  def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:event_errors] = @event.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :body)
   end
 end
