61b20c450f980e1c48480da8302264f6f139b3fe
megpeer <148973963+megpeer@users.noreply.github.com>
2025-05-25 17:42:48 -0400

    separated past events + future events scopes in model. allowing for separated collections of past + upcomming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  scope :past_events, -> { where('date < ?', Time.zone.now) }
+  scope :future_events, -> { where('date > ?', Time.zone.now) }
+
   belongs_to :user
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
